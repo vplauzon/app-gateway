63cb23a8c2bb545ea463a0c3854fe06eaaf4f415
vplauzon <vincent_lauzon@yahoo.com.au>
2018-05-09 16:09:24 -0400

    .

--- multi-vnets-vmss-arm/scripts/DeployDsc.ps1
+++
@@ -1,3 +0,0 @@
-#  Script to create the DSC on a storage account
-
-Publish-AzureRmVMDscConfiguration -ConfigurationPath "< CorpWeb.ps1 full path>" -ResourceGroupName Demo-Set -StorageAccountName <Storage Account Name> -Force
